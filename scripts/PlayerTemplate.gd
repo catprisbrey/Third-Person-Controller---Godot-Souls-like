35f71dcf4d5200ffaf221f2c6264fffa88efc3c0
Cat <43008564+pemguin005@users.noreply.github.com>
2022-06-28 16:26:04 -0700

    Cleaner state changes
    Noticed some poor defining of states which could lead to inconsistent advance conditions. Added more clearly defined "else" statements to keep states more consistently on/off to prevent mishaps.

--- scripts/PlayerTemplate.gd
+++ scripts/PlayerTemplate.gd
@@ -1,9 +1,7 @@
 extends KinematicBody
 
-
-
 # Allows to pick your animation tree from the inspector
-export (NodePath) var PlayerAnimationTree
+export (NodePath) var PlayerAnimationTree 
 export onready var animation_tree = get_node(PlayerAnimationTree)
 onready var playback = animation_tree.get("parameters/playback");
 
@@ -11,12 +9,6 @@ onready var playback = animation_tree.get("parameters/playback");
 export (NodePath) var PlayerCharacterMesh
 export onready var player_mesh = get_node(PlayerCharacterMesh)
 
-# Gamplay mechanics and Inspector tweakables
-export var gravity = 9.8
-export var jump_force = 9
-export var walk_speed = 2
-export var run_speed = 5
-export var dash_power = 12 # Controls roll and big attack speed boosts
 # Animation node names
 var roll_node_name = "Roll"
 var idle_node_name = "Idle"
@@ -26,6 +18,13 @@ var attack1_node_name = "Attack1"
 var attack2_node_name = "Attack2"
 var bigattack_node_name = "BigAttack"
 
+# Condition States
+var is_attacking = bool()
+var is_rolling = bool()
+var is_walking = bool()
+var is_running = bool()
+
+# Physics values
 var direction = Vector3()
 var horizontal_velocity = Vector3()
 var aim_turn = float()
@@ -33,17 +32,22 @@ var movement = Vector3()
 var vertical_velocity = Vector3()
 var movement_speed = int()
 var angular_acceleration = int()
-var is_attacking = bool()
-var is_rolling = bool()
+var acceleration = int()
 
+# Gamplay mechanics and Inspector tweakables
+export var gravity = 9.8
+export var jump_force = 9
+export var walk_speed = 2
+export var run_speed = 5
+export var dash_power = 12 # Controls roll and big attack speed boosts
 
 func _ready(): # Camera based Rotation
 	direction = Vector3.BACK.rotated(Vector3.UP, $Camroot/h.global_transform.basis.get_euler().y)
 
 func _input(event): # All major mouse and button input events
 	if event is InputEventMouseMotion:
-		aim_turn = -event.relative.x * 0.015 # animates player with mouse movement while aiming
-
+		aim_turn = -event.relative.x * 0.015 # animates player with mouse movement while aiming 
+	
 	if event.is_action_pressed("aim"): # Aim button triggers a strafe walk and camera mechanic
 		direction = $Camroot/h.global_transform.basis.z
 
@@ -67,46 +71,39 @@ func _input(event): # All major mouse and button input events
 		if run_node_name in playback.get_current_node() and event.is_action_pressed("attack"): # Big Attack if sprinting, adds a dash
 			horizontal_velocity = direction * dash_power
 			playback.travel(bigattack_node_name)
-
-#	Final attack lines, prevent node travel during final attacks, stopping attack1 from happening immediately after other attack combos finish.
-		if (bigattack_node_name in playback.get_current_node()) or (attack2_node_name in playback.get_current_node()) and event.is_action_pressed("attack"):
-			playback.travel(idle_node_name)
-
-
+	
 func _physics_process(delta):
-
-	var is_attacking = false
-	var is_walking = false
-	var is_running = false
-	var is_rolling = false
-	var on_floor = is_on_floor() # AKA is jumping/falling/landing
-	var movement_speed = 0
-	var angular_acceleration = 10
-	var acceleration = 15
-
+	
+	var on_floor = is_on_floor() # State control for is jumping/falling/landing
 	var h_rot = $Camroot/h.global_transform.basis.get_euler().y
+	
+	movement_speed = 0
+	angular_acceleration = 10
+	acceleration = 15
 
 	# Gravity mechanics and prevent slope-sliding
 	if not is_on_floor(): vertical_velocity += Vector3.DOWN * gravity * 2 * delta
 	else: vertical_velocity = -get_floor_normal() * gravity / 3
-
+	
 	# Defining attack state: Add more attacks here if you like
-	if (attack1_node_name in playback.get_current_node()) or (attack2_node_name in playback.get_current_node()) or (bigattack_node_name in playback.get_current_node()):
+	if (attack1_node_name in playback.get_current_node()) or (attack2_node_name in playback.get_current_node()) or (bigattack_node_name in playback.get_current_node()): 
 		is_attacking = true
+	else: is_attacking = false
 
 # Giving BigAttack some Slide
 	if bigattack_node_name in playback.get_current_node(): acceleration = 3
 
 	# Defining Roll state and limiting movment during rolls
-	if roll_node_name in playback.get_current_node():
+	if roll_node_name in playback.get_current_node(): 
 		is_rolling = true
 		acceleration = 2
 		angular_acceleration = 2
-
+	else: is_rolling = false
+	
 	# Jump input and Mechanics
 	if Input.is_action_just_pressed("jump") and ((is_attacking != true) and (is_rolling != true)) and is_on_floor():
 		vertical_velocity = Vector3.UP * jump_force
-
+		
 	# Movement input, state and mechanics. *Note: movement stops if attacking
 	if (Input.is_action_pressed("forward") ||  Input.is_action_pressed("backward") ||  Input.is_action_pressed("left") ||  Input.is_action_pressed("right")):
 		direction = Vector3(Input.get_action_strength("left") - Input.get_action_strength("right"),
@@ -115,24 +112,26 @@ func _physics_process(delta):
 		direction = direction.rotated(Vector3.UP, h_rot).normalized()
 		is_walking = true
 	# Sprint input, state and speed
-		if Input.is_action_pressed("sprint"):
+		if Input.is_action_pressed("sprint"): 
 			movement_speed = run_speed
 			is_running = true
 		else: # Walk State and speed
 			movement_speed = walk_speed
+			is_running = false
+	else: is_walking = false
 
 	if Input.is_action_pressed("aim"):  # Aim/Strafe input and  mechanics
 		player_mesh.rotation.y = lerp_angle(player_mesh.rotation.y, $Camroot/h.rotation.y, delta * angular_acceleration)
 
 	else: # Normal turn movement mechanics
 		player_mesh.rotation.y = lerp_angle(player_mesh.rotation.y, atan2(direction.x, direction.z) - rotation.y, delta * angular_acceleration)
-
+	
 	# Movment mechanics with limitations during rolls/attacks
-	if ((is_attacking == true) or (is_rolling == true)):
+	if ((is_attacking == true) or (is_rolling == true)): 
 		horizontal_velocity = horizontal_velocity.linear_interpolate(direction.normalized() * .01 , acceleration * delta)
-	else: # Movement mechanics without limitations
+	else: # Movement mechanics without limitations 
 		horizontal_velocity = horizontal_velocity.linear_interpolate(direction.normalized() * movement_speed, acceleration * delta)
-
+	
 	# The Sauce. Movement, gravity and velocity in a perfect dance.
 	movement.z = horizontal_velocity.z + vertical_velocity.z
 	movement.x = horizontal_velocity.x + vertical_velocity.x
@@ -140,9 +139,9 @@ func _physics_process(delta):
 	move_and_slide(movement, Vector3.UP)
 
 	# ========= State machine controls =========
-	# The booleans of the on_floor, is_walking etc, trigger the
+	# The booleans of the on_floor, is_walking etc, trigger the 
 	# advanced conditions of the AnimationTree, controlling animation paths
-
+	
 	# on_floor manages jumps and falls
 	animation_tree["parameters/conditions/IsOnFloor"] = on_floor
 	animation_tree["parameters/conditions/IsInAir"] = !on_floor
@@ -153,5 +152,5 @@ func _physics_process(delta):
 	animation_tree["parameters/conditions/IsNotRunning"] = !is_running
 	# Attacks and roll don't use these boolean conditions, instead
 	# they use "travel" or "start" to one-shot their animations.
-
-
+	
+	
