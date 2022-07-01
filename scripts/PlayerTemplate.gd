60d612dab9f06b47ad15b60b5dce7a07f1fba93e
Cat <catprisbrey@gmail.com>
2022-07-01 11:43:40 -0700

    Asset overhaul
    Rebuilt player model merging materials in order to delete all unused materials and clean up the project.

--- scripts/PlayerTemplate.gd
+++ scripts/PlayerTemplate.gd
@@ -12,8 +12,8 @@ export onready var player_mesh = get_node(PlayerCharacterMesh)
 # Gamplay mechanics and Inspector tweakables
 export var gravity = 9.8
 export var jump_force = 9
-export var walk_speed = 2
-export var run_speed = 5
+export var walk_speed = 1.3
+export var run_speed = 5.5
 export var dash_power = 12 # Controls roll and big attack speed boosts
 
 # Animation node names
